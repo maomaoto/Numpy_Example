bf2c5a3e5f33469992804d410a62713d1e248b8c
Wei-Lun CHANG <maomaoto@gmail.com>
2015-08-30 01:00:15 +0800

    Add Ch5 sample codes

---
+++ Ch5/answer42.py
@@ -0,0 +1,13 @@
+#! usr/bin/env/python
+
+import numpy as np
+
+def ultimate_answer(a):
+	result = np.zeros_like(a)
+	result.flat = 42
+	
+	return result
+
+ufunc = np.frompyfunc(ultimate_answer, 1, 1)
+print("The answer {0}".format(ufunc(np.arange(4))))
+print("The answer {0}".format(ufunc(np.arange(4).reshape(2,2))))
